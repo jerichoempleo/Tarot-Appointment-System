e355213a7c69a822158927923e47afb200240a6b
jerichoempleo <92818604+jerichoempleo@users.noreply.github.com>
2024-07-21 15:20:39 +0800

    jwt

---
+++ backend/Dto/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TarotAppointment.Dto
+{
+    public class AuthResponseDto
+    {
+        public string? Token { get; set; } = string.Empty;
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ backend/Dto/CreateRoleDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TarotAppointment.Dto
+{
+    public class CreateRoleDto
+    {
+        [Required(ErrorMessage = "Role Name is required.")]
+        public string RoleName { get; set; } = null!;
+    }
+}
